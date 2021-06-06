07b2a67880a0653d51a6d86e9d4544fc57ccf7a4
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-06 22:00:00 +0530

    weather api pro complete

--- weather_api/server.js
+++ weather_api/server.js
@@ -1,5 +1,6 @@
     const express = require("express");
     const https = require("https");
+    const bodyParser = require("body-parser");
     const fs = require("fs");
 
     const app = express();
@@ -7,10 +8,17 @@
 
     app.set("view engine", "ejs");
     app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 
     app.get('/', (req, res) => {
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=gandhidham&appid=437ee22a40010f2b8b8f15b171f3272d#";
+
+        const apikey = "437ee22a40010f2b8b8f15b171f3272";
+        var api_loc = req.body.location ? req.body.location : "rajkot";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${api_loc}&appid=${apikey}d&units=metric`;
+
 
         https.get(url, (response) => {
             // console.log(response.statusCode);
@@ -33,26 +41,70 @@
                 const loc = weather_data.name;
                 //weather
                 const cond = weather_data.weather[0].description;
-            })
-        })
 
-        // if (req.url == '/') {
-        //     requests("http://api.openweathermap.org/data/2.5/weather?q=gandhidham&appid=437ee22a40010f2b8b8f15b171f3272d")
-        //         .on("data", (chunk) => {
-        //             const obj = JSON.parse(chunk);
-        //             //  cont arr = [obj];
-        //             console.log(obj);
-        //         })
-        //         .on("end", (err) => {
-        //             if (err);
-        //             return
-        //         })
-        // }
+                res.render("index", {
+                    title: "WEATHER API",
+                    ht_max: max,
+                    ht_min: min,
+                    ht_temp: temp,
+                    ht_feel: feel,
+                    ht_country: country,
+                    ht_loc: loc,
+                    ht_wind_speed: wind_speed,
+                    ht_wind_degree: wind_degree,
+                    ht_press: press,
+                    ht_humid: humid,
+                });
+            });
+        });
+    });
 
-        res.render("index");
-    })
+    app.post('/', (req, res) => {
+        console.log("Post Request Recived..!");
+        const apikey = "437ee22a40010f2b8b8f15b171f3272";
+        const api_loc = req.body.location ? req.body.location : "rajkot";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${api_loc}&appid=${apikey}d&units=metric`;
 
+
+        https.get(url, (response) => {
+            // console.log(response.statusCode);
+            response.on("data", (data) => {
+                const weather_data = JSON.parse(data);
+                // console.log(weather_data);
+                //temp
+                const temp = weather_data.main.temp;
+                const feel = weather_data.main.feels_like;
+                const min = weather_data.main.temp_min;
+                const max = weather_data.main.temp_max;
+                //humid & pressure 
+                const humid = weather_data.main.humidity;
+                const press = weather_data.main.pressure;
+                //wind
+                const wind_speed = weather_data.wind.speed;
+                const wind_degree = weather_data.wind.deg;
+                //location
+                const country = weather_data.sys.country;
+                const loc = weather_data.name;
+                //weather
+                const cond = weather_data.weather[0].description;
+
+                res.render("index", {
+                    title: "WEATHER API",
+                    ht_max: max,
+                    ht_min: min,
+                    ht_temp: temp,
+                    ht_feel: feel,
+                    ht_country: country,
+                    ht_loc: loc,
+                    ht_wind_speed: wind_speed,
+                    ht_wind_degree: wind_degree,
+                    ht_press: press,
+                    ht_humid: humid,
+                });
+            });
+        });
+    });
 
     app.listen(port, () => {
-        console.log(`Your Server Has Started at Port No:- ${port}`)
+        console.log(`Your Server Has Started at Port No:- ${port}`);
     })
